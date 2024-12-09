4a1bda8e165c27f20e411ca93ba48f09566a586c
pedrodese <pedrohenriquedasilvapereira12@gmail.com>
2024-12-09 01:41:10 -0300

    Aula sobre loops dentro do GO

---
+++ Loops/loops.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// lembrando que o Go só tem apenas um laço de repetição que é o FOR que pode ser usado de diferentes formas
+func main() {
+	fmt.Println("Loops de repetição")
+
+	i := 0
+
+	for i < 10 {
+		i++
+		fmt.Println("Incrementando I")
+		time.Sleep(time.Second)
+	}
+
+	fmt.Println(i)
+
+	for j := 0; j < 10; j++ {
+		fmt.Println("Incrementando J ", j)
+	}
+
+	//forma de iterar algo, parecido com o foreach do Java
+	nomes := []string{"Pedro", "Rafaela", "Kelcy"}
+	for indice, nome := range nomes {
+		fmt.Println(indice, nome)
+	}
+
+	//posso tambem iterar uma string percorrendo ela e retornando os indices e os valores
+	for indice, letra := range "PALAVRA" {
+		fmt.Println(indice, string(letra))
+	}
+}
